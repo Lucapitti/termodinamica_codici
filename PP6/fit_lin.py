4e3d7a02c622273e63981d279bee398734fe7461
Luca Pittigliani <luca@MacBook-Air-di-Luca.fritz.box>
2026-01-02 12:53:14 +0100

    ciao

--- PP6/fit_lin.py
+++ PP6/fit_lin.py
@@ -8,12 +8,12 @@ uP0 = 0.5
 offset = 0.15
 uOffset = 0.003
 
-Delta_pressioni = np.array([-89.79, -90.08, -90.20, -89.53])
-Temp_multiple = np.array([49, 50, 50.6, 51.8])
+Delta_pressioni = np.array([-89.30, -89.33, -89.53])
+Temp_multiple = np.array([50, 50.6, 51.8])
 uTemp = np.std(Temp_multiple, ddof=1)
 uPress = np.std(Delta_pressioni, ddof=1)
 Temp = np.array([np.mean(Temp_multiple), 56, 59.4, 64.8, 69.8, 74.4, 79.6, 84.6, 90.4]) + 273.15
-Press = np.array([np.mean(Delta_pressioni), -86.41, -83.39, -78.26, -72.56, -67.90, -59.69, -50.61, -36.73])
+Press = np.array([np.mean(Delta_pressioni), -85.88, -83.39, -78.26, -72.56, -67.59, -59.69, -50.01, -36.88])
 Press = (np.array(Press) - offset + P0) *1000
 uPress = np.array(uPress)
 uTemp = np.array(uTemp)
@@ -24,8 +24,24 @@ x = 1/Temp
 uy = uPress/Press
 ux = uTemp/Temp**2
 
+print(uPress, uTemp)
+
 m, um, c, uc, cov, rho = my.lin_fit(x, y, uy)
 m, um, c, uc, cov, rho = my.lin_fit(x, y, np.sqrt(uy**2 + (m*ux)**2), plot=True)
+plt.show()
+
+y_pred = x * m + c
+res = y_pred - y
+res_norm = res / np.sqrt(uy**2 + (m*ux)**2)
+
+plt.figure()
+plt.axhline(0, lw=0.8, color='black')
+plt.errorbar(y, res_norm, yerr=np.ones_like(res_norm), fmt='o')
+plt.title("Residui normalizzati")
+plt.xlabel("$\Delta T / T$")
+plt.ylabel("Residui normalizzati")
+plt.grid(True)
+plt.savefig(f"residui_pres_temp.png")
 plt.show()
 
 print(-m*8.314/18)
